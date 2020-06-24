a6ee976ba968f555359a017ac2c3fa2dad2a7a1c
Flor Rodriguez <florrod23@gmail.com>
2020-06-24 18:58:01 +0000

    Ejercicio resuelto

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,21 @@
-/* eslint-disable */
-
-import "file-loader?name=[name].[ext]!../index.html";
 import "../assets/img/rigo-baby.jpg";
 import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-/*window.onload = function() {
-  var queryPrueba = document.querySelector(".prueba");
-
-  console.log(queryPrueba);
-};*/
-var numbersList = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-var seedList = ["diamonds", "spades", "clubs", "hearts"];
-
-window.onload = () => {
-  createCard();
+window.onload = function() {
+  this.document.querySelector(".card").classList.add(getRandomSeed());
+  this.document.querySelector(".card").innerHTML = getRandomNumber();
 };
 
-function createCard() {
-  var number = getRandomNumber();
-  console.log(number);
-  var seed = getRandomSeed();
-  document.querySelectorAll("span")[0].innerHTML = number;
+let getRandomNumber = () => {
+  let numbers = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
 
-  document.querySelectorAll("p")[0].classList.add(seed);
-  console.log(document.querySelectorAll("p")[0].classList);
-}
-
-function getRandomNumber() {
-  return numbersList[Math.floor(Math.random() * numbersList.length)];
-}
-
-function getRandomSeed() {
-  return seedList[Math.floor(Math.random() * seedList.length)];
-}
+let getRandomSeed = () => {
+  let seed = ["diamond", "spade", "club", "heart"];
+  let indexSeed = Math.floor(Math.random() * seed.length);
+  return seed[indexSeed];
+};
